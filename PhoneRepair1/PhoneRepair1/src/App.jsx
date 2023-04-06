8f7b574c1306b94c6167f123d15d26b03e317ec1
INFINITYVDI006\infinitydev <getasewmequanint@cbe.com.et>
2023-04-06 19:19:24 +0300

    Create and add new branch with new react app

---
+++ PhoneRepair1/PhoneRepair1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App bg-[#79d2e3] ">
+      ifixandrepair
+    </div>
+  )
+}
+
+export default App
